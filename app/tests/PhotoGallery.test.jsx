b783ce662c9b74a0207c5d17b91166092d6d09ce
Kevin Carr <cochewen@gmail.com>
2017-03-06 10:31:56 -0500

    added minor test

---
+++ app/tests/PhotoGallery.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+var PhotoGallery = require('PhotoGallery');
+
+
+describe('PhotoGallery', () => {
+  it('should exist', () => {
+    expect(PhotoGallery).toExist();
+  });
+});
--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 
+
 describe('App', () => {
   it('should properly run tests', () => {
     expect(1).toBe(1);
